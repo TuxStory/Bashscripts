a3902f7e58f91761ee0b54a43576de55dfa52beb
TuxStory <ajm.even@gmail.com>
2020-08-13 15:46:16 +0200

    Debian

--- SystemCheck.sh
+++ SystemCheck.sh
@@ -18,6 +18,7 @@ echo -e "			System Check				"
 echo "=================================================================="
 echo
 echo -n "UPTIME : ";  uptime -p
+echo "Journalctl Disk Usage : "; journalctl --disk-usage
 echo
 echo "--------------------------- who ---------------------------------"
 who
@@ -27,17 +28,25 @@ echo "-------------------------- last ---------------------------------"
 last | head
 #echo "-----------------------------------------------------------------"
 #blast
+
+echo "-------------------------- Login  --------------------------------"
+if [ -e /etc/debian_version ]
+then
+	tail -n 20 /var/log/auth.log
+else
+	#echo "--------------------- Acces aux fichier ------------------------"
+	#lsof +D /home/$USER/
+	echo "---------------------------- Login -------------------------------"
+	#Fedora pas de /var/log/auth.log
+	journalctl -q _AUDIT_TYPE=1112 _TRANSPORT=audit | grep USER_LOGIN --color
+	echo "------------------------- Login failed ---------------------------"
+	journalctl -q _AUDIT_TYPE=1112 _TRANSPORT=audit | grep failed --color
+	echo "------------------------- ssh Login --------------------------------"
+	journalctl -q _AUDIT_TYPE=1112 _TRANSPORT=audit | grep sshd --color
+fi
+
 echo "------------------ Connection Reseau active ---------------------"
 lsof -i
-#echo "--------------------- Acces aux fichier ------------------------"
-#lsof +D /home/$USER/
-echo "------------ processus qui écoutent sur le port 80 --------------"
+#echo "------------ processus qui écoutent sur le port 80 --------------"
 #sudo lsof -nP -i tcp:80 | grep LISTEN
-lsof -nP -i tcp:80
-echo "---------------------------- Login -------------------------------"
-#Fedora pas de /var/log/auth.log
-journalctl -q _AUDIT_TYPE=1112 _TRANSPORT=audit | grep USER_LOGIN --color
-echo "------------------------- Login failed ---------------------------"
-journalctl -q _AUDIT_TYPE=1112 _TRANSPORT=audit | grep failed --color
-echo "------------------------- ssh Login --------------------------------"
-journalctl -q _AUDIT_TYPE=1112 _TRANSPORT=audit | grep sshd --color
+#lsof -nP -i tcp:80
