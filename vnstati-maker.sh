84bb3b8217f76e058fc99cf1b0272b7a9c81fb58
TuxStory <ajm.even@gmail.com>
2020-09-18 00:10:52 +0200

    ajouts

--- vnstati-maker.sh
+++ vnstati-maker.sh
@@ -4,22 +4,33 @@
 # Script : vnstati-maker.sh #
 # Auteur : Antoine Even     #
 # Date   : 16/09/2020       #
+# Modif  : 17/09/2020       #
 #############################
 
-#Variables
+#Configuration
+INTERFACE="wlan0"
+DOSSIER="/home/$USER/vnstat-graph"
+VERSION="0.2"
 
+#Variables
 DATE=$(date +"%d-%m-%Y")
+JOUR=$(date +"%d")
 MOIS=$(date +"%B")
 ANNEE=$(date +"%Y")
 
-DOSSIER="/home/$USER/vnstat-graph"
-INTERFACE="wlan0"
-
+#Vérification du dossier.
 if [ ! -d $DOSSIER ]; then
 mkdir $DOSSIER
 fi
 
+#Creation PNG du jour
 cd $DOSSIER
 vnstati -vs -i $INTERFACE -o Jour-$DATE.png
-vnstati -m -i $INTERFACE -o $MOIS-$ANNEE.png
-vnstati -d -i $INTERFACE -o $MOIS-$ANNEE-DETAIL.png
+
+if [ $JOUR == 1 ]; then
+    vnstati -m -i $INTERFACE -o $MOIS-$ANNEE.png
+    vnstati -d -i $INTERFACE -o $MOIS-$ANNEE-DETAIL.png
+fi
+
+#Décommenter pour les Stats par années
+#vnstati -y -i wlan0 -o Année.png
