6391d374cdbb9e4246f30876c4e190be3ee9f547
TuxStory <ajm.even@gmail.com>
2020-10-21 00:10:34 +0200

    ajout couleur

--- Purge_Debian.sh
+++ Purge_Debian.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "Recherche de résidus ..."
-
+echo -e "\033[01;32m>>> \033[00mRecherche de résidus ... "
 [[ $(dpkg -l | grep ^rc) ]] && sudo dpkg -P $(dpkg -l | awk '/^rc/{print $2}') || echo "Aucun résidu trouvé."
