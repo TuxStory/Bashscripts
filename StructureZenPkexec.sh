539bd07ea7c3b02d3be25a987f94230aba2d5689
TuxStory <ajm.even@gmail.com>
2020-10-28 14:09:15 +0100

    Structure Zen Pkexec

---
+++ StructureZenPkexec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MAJ()
+{
+  pkexec apt update && pkexec apt upgrade -y
+}
+
+zenity --question --width=200 --title="Debian 10 : updates" --text="\nInstaller les mises à jours"
+if [ $? = 0 ] ; then
+  echo "OUI !"
+   MAJ | zenity --progress --text "Installation des mises à jours." --auto-close --pulsate --width=300
+else
+  echo "NON !"
+  exit 1
+fi
