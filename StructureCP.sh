c164bc684bb63811a8ca2f61027b51552f78910d
TuxStory <ajm.even@gmail.com>
2020-10-29 00:33:04 +0100

    Ajout test

--- StructureCP.sh
+++ StructureCP.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 Fich=1
-Dossier1="/home/antoine/Bureau/Mp3/"
+Dossier1="/home/antoine/Bureau/RootMe/"
 Dossier2="/home/antoine/Bureau/CopeiTEST/"
-Nombre=$(ls -R $Dossier1 | wc -l)
-echo $Nombre
+Nombre=$(ls $Dossier1 | wc -l)
+#echo $Nombre
 # Pour éviter le probleme des espaces dans les nom de dossiers
 IFS='
 '
 
 copie()
 {
-  for fichier in $Dossier1/*/*
+  for fichier in $Dossier1/*
   do
-    POURC=$Fich*100/$NOMBRE
+    POURC=$Fich*100/$Nombre
     echo "$POURC"
     echo "# copie de $(basename $fichier)"
     cp -rf $fichier $Dossier2
@@ -21,7 +21,7 @@ copie()
   done
 }
 
-zenity --question --width=200 --title="Backup" --text="\nFaut-il faire le backup"
+zenity --question --width=200 --title="Backup" --text="\nFaut-il faire le backup ?"
 if [ $? = 0 ] ; then
   copie | zenity --progress --percentage=0 --width=400 --auto-close
 else
