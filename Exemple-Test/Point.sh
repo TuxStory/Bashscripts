bd83c96b160fe7cfa4224e3749935cbabc454737
TuxStory <ajm.even@gmail.com>
2020-11-07 22:12:19 +0100

    Correction Point

--- Exemple-Test/Point.sh
+++ Exemple-Test/Point.sh
@@ -5,13 +5,14 @@ tput civis
 
 for ((l=0 ; l<3; l++))
 do
-  for i in {0..20} {19..0}
+  for i in {0..20} {20..0}
   do
     for j in {0..20}
       do
       if [ $j -eq $i ] ; then
-        echo -n "."
+        echo -n ". " #+1 espace pour effacer le poit precedent au retour.
         sleep 0.1
+        #echo $i $j
       else
         echo -n " "
       fi
