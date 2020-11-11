c6683d61a7ebc2254cad1f3a8de609bc9894a5ab
TuxStory <ajm.even@gmail.com>
2020-11-11 02:13:36 +0100

    Test Upper

--- Exemple-Test/UpperLower.sh
+++ Exemple-Test/UpperLower.sh
@@ -8,8 +8,41 @@ do
   sleep 1
 done
 
-STR="This is a Test"
-for L in $(seq 1 ${#STR}); do
-    echo $(echo $STR | cut -c$L | tr '[:lower:]' '[:upper:]' )
-    sleep 0.5
+for i in {1..5} ; do
+  #STR="This is a Test"
+  STR="Debian10Buster"
+  for L in $(seq 1 ${#STR}); do
+      if [[ $STR == [A-Z] ]]; then
+        echo -n $(echo $STR | cut -c$L | tr '[:upper:]' '[:lower:]' )
+        sleep 0.2
+      else
+        echo -n $(echo $STR | cut -c$L | tr '[:lower:]' '[:upper:]' )
+        sleep 0.2
+      fi
+  done
+  echo -ne "\r\033[K"
 done
+echo
+
+#$ name='test'
+#$ echo $name
+#$ echo ${name^} > Test
+#$ echo ${name^^} > TEST
+
+#2 To actually clear the line for the new text, you can add \033[K after the \r:
+
+STR="debian10buster"
+for i in {1..5} ; do
+  for L in $(seq 1 ${#STR}); do
+    echo -n $(echo $STR | cut -c$L)
+    sleep 0.2
+  done
+  echo -ne "\r\033[K"
+  if [[ $STR == [a*-z*] ]]; then
+    STR=${STR^^}
+  else
+    STR="debian10buster"
+  fi
+  echo $STR
+done
+echo
