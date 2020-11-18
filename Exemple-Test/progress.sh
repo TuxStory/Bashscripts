dd70d04bd0fe85b596b32326728854e680fea824
TuxStory <ajm.even@gmail.com>
2020-11-18 23:58:44 +0100

    test progress

--- Exemple-Test/progress.sh
+++ Exemple-Test/progress.sh
@@ -11,3 +11,15 @@ do
     sleep 0.05
 done
 echo
+
+for ((k = 0; k <= 100 ; k++))
+do
+    echo -n "[ "
+    for ((i = 0 ; i <= k; i++)); do echo -n "#"; done
+    for ((j = i ; j <= 10 ; j++)); do echo -n " "; done
+    v=$((k * 1))
+    echo -n " ] "
+    echo -n "$v %" $'\r'
+    sleep 0.05
+done
+echo
