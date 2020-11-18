a5e1286e75c3b9fa5e324d08373f7e7ed3d2dccb
TuxStory <ajm.even@gmail.com>
2020-11-19 00:11:46 +0100

    progress finish

--- Exemple-Test/progress.sh
+++ Exemple-Test/progress.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tput civis #cache le curseur
+
 for ((k = 0; k <= 10 ; k++))
 do
     echo -n "[ "
@@ -16,10 +18,12 @@ for ((k = 0; k <= 100 ; k++))
 do
     echo -n "[ "
     for ((i = 0 ; i <= k; i++)); do echo -n "#"; done
-    for ((j = i ; j <= 10 ; j++)); do echo -n " "; done
+    for ((j = i ; j <= 100 ; j++)); do echo -n " "; done
     v=$((k * 1))
     echo -n " ] "
     echo -n "$v %" $'\r'
     sleep 0.05
 done
 echo
+
+tput cvvis
