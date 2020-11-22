eb6f3c7ebb443e442c201678dc7422419396f980
TuxStory <ajm.even@gmail.com>
2020-11-22 22:59:19 +0100

    colors

--- Exemple-Test/TXTest.sh
+++ Exemple-Test/TXTest.sh
@@ -50,6 +50,13 @@ echo "${fgBlue}Blue Test"
 tput sgr0
 echo "${bgYellow}Yellow Test"
 tput sgr0
+echo "${bgBlue}Test Blue"
+tput sgr0
+echo "${fgBlue}Blue Test"
+tput sgr0
+echo "${txReset} "
+echo test
+tput sgr0
 
 printf "Your username is %s\n" $USER
 echo Your username is $USER
@@ -61,3 +68,25 @@ sleep 0.1
 }
 
 echo
+echo -e "\033[31mUn texte en rouge,\033[0m la suite ne l'est plus"
+echo -e "Un texte en \e[32m\033[43mvert sur fond jaune\E[0m"
+echo -e "Et voici \033[31m\033[1m\033[4m\033[5m\033[7mun texte\033[0m mis en forme selon une forme particulièrement peu explicite."
+
+#!/bin/bash
+couleurs=(Noir Rouge Vert Jaune Bleu Magenta Cyan Blanc);
+for i in $(seq 1 7);
+do
+  echo -e " $(tput setaf $i)${couleurs[$i]}\tTexte$(tput sgr0)\t$(tput bold)$(tput setaf $i)Texte$(tput sgr0)\t$(tput sgr 0 1)$(tput setaf $i)Texte$(tput sgr0)\t(tput setaf $i)"; 
+done
+
+echo
+
+for i in {0..256}
+do
+  o=00$i
+  echo -ne "${o:${#o}-3:3} "$(tput setaf $i;tput setab $i)"   "$(tput sgr0)
+done
+
+echo
+
+#https://doc.ubuntu-fr.org/tutoriel/formater_le_texte_dans_le_terminal_avec_tput
