0579366940f8d3e8f4e7675199ab3cc8b7d18716
TuxStory <ajm.even@gmail.com>
2020-12-12 02:47:02 +0100

    regex debian

---
+++ Getdebian.sh
@@ -0,0 +1,11 @@
+#include
+
+REGEX="https:\/\/[a-z]{7}.*.iso"
+Link=$(wget -qO- www.debian.org | grep -Eo $REGEX)
+echo $Link
+wget $Link -P ~/
+
+#wget -qO- www.debian.org |
+#grep -Eoi '<a [^>]+>' | 
+#grep -Eo 'href="[^\"]+"' | 
+#grep -Eo '(http|https)://[^/"]+'
