6e00a02fa382c75145be2f1dd4aef377b2ab4d3e
TuxStory <ajm.even@gmail.com>
2021-07-02 10:13:41 +0200

    motalea

--- motalea.sh
+++ motalea.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
+#Script Adrien LinuxTricks
 
 # Chemin du dictionnaire
-#DICT="/usr/share/hunspell/fr-moderne.dic"
+#DICT="/usr/share/hunspell/fr-moderne.dic" #Pas dispo chez moi
 DICT="/usr/share/hunspell/fr.dic"
 
 # Calcul du nb de lignes du fichier
 NBLIGNES=$(grep -c -E '^' $DICT)
 
-# Génération d'un nb aléatoire (pour une ligne au pif du fichier)
+# Génération d'un nb aléatoire
 ALEA=$(shuf -i 1-$NBLIGNES -n 1)
 
 # Sélection de la ligne concernée
