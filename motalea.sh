37b76864ff64a9bd442837e570120ef90b5cedc4
TuxStory <ajm.even@gmail.com>
2021-07-02 10:09:40 +0200

    motalea

---
+++ motalea.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Chemin du dictionnaire
+#DICT="/usr/share/hunspell/fr-moderne.dic"
+DICT="/usr/share/hunspell/fr.dic"
+
+# Calcul du nb de lignes du fichier
+NBLIGNES=$(grep -c -E '^' $DICT)
+
+# Génération d'un nb aléatoire (pour une ligne au pif du fichier)
+ALEA=$(shuf -i 1-$NBLIGNES -n 1)
+
+# Sélection de la ligne concernée
+MOT=$(head -n $ALEA $DICT | tail -1)
+
+# Epuration des caractères bizarres
+MOT=$(echo $MOT | cut -d '/' -f 1)
+MOT=$(echo $MOT | awk '{print $1}')
+
+# Affichage du mot sur la console
+echo $MOT
