5c93fed20b4fd1c15a35aa07a5c6055f941878b8
TuxStory <ajm.even@gmail.com>
2022-07-03 00:24:30 +0200

    comment

---
+++ Exemple-Test/setetest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+A=10
+
+set +e
+#set -e
+
+echo "Hello World !"
+error
+echo test
+echo $A
+
+<<"EXPLICATION"
+antoine@Bullseye:~/GitHub/Bashscripts/Exemple-Test$ ./setetest.sh 
+Hello World !
+./setetest.sh: ligne 6: error : commande introuvable
+antoine@Bullseye:~/GitHub/Bashscripts/Exemple-Test$ nano setetest.sh 
+antoine@Bullseye:~/GitHub/Bashscripts/Exemple-Test$ ./setetest.sh 
+Hello World !
+./setetest.sh: ligne 6: error : commande introuvable
+test
+10
+EXPLICATION
+
+: '
+comment1comment1
+comment2comment2
+comment3comment3
+comment4comment4
+'
--- Exemple-Test/trapsete.sh
+++ Exemple-Test/trapsete.sh
@@ -7,3 +7,7 @@ echo "error at $1"
 trap 'error $LINENO' ERR
 
 errors
+
+#Set –e is used within the Bash to stop execution instantly as a query
+#exits while having a non-zero status. This function is also used when
+#you need to know the error location in the running code.
