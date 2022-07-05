94eca3f352bfdaec97318a1f34d498634e4b566c
TuxStory <ajm.even@gmail.com>
2022-07-06 00:35:12 +0200

    array

---
+++ Exemple-Test/array.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+names=('riri' 'fifi' 'loulou')
+echo ${names[@]}
+names+=('Picsou')
+echo ${names[@]}
+echo ${#names[@]}
+echo ${names[0]}
+echo ${names[1]}
+
+echo "===== loop ====="
+for str in ${names[@]}; do
+  echo $str
+done
+
+echo "===== loop 2====="
+for i in ${!names[@]}; do
+  echo "element $i is ${myArray[$i]}"
+done
+
+echo "===== Removes ====="
+names+=('Donald')
+unset names[3]
+echo ${names[@]}
+
+echo ===== Upper Case =====
+echo ${names[1]^}
+echo ${names[1]^^}
