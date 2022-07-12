9b3bb4a2d1077ea767d14c9e8996a3651688527b
TuxStory <ajm.even@gmail.com>
2022-07-12 17:31:21 +0200

    test

--- Exemple-Test/function_test.sh
+++ Exemple-Test/function_test.sh
@@ -3,6 +3,7 @@
 function Test ()
 {
 TEST=$(cat /etc/debian_version)
+
 return 2
 }
 
@@ -11,7 +12,25 @@ function Test2 ()
 echo $TEST
 }
 
+function OS ()
+{
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+    ID=$ID
+    VARIANT=$VARIANT
+  fi
+}
+
+
+echo $TEST
 Test
 echo $?
 Test2
 echo $?
+
+OS
+echo "-------- Data --------"
+echo -e " $NAME \n $VERSION_ID \n $ID \n $VARIANT"
+echo "----------------------"
