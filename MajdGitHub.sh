f32d2ed103617706d2ba2b76c01c2a295a9462a4
TuxStory <ajm.even@gmail.com>
2022-08-07 20:23:34 +0200

    check

--- MajdGitHub.sh
+++ MajdGitHub.sh
@@ -3,14 +3,14 @@
 ################################
 # Auteur	: Antoine Even #
 # Nom		: MajdGitHub   #
-# Date		: 18/10/20202  #
-# Revision	: 18/10/2020   #
-# Version	: 0.1	       #
+# Date		: 18/10/2020   #
+# Revision	: 07/08/2022   #
+# Version	: 0.2	       #
 ################################
 
 cd ../
 for d in */ ; do
   echo -e "\033[01;32m>>> \033[00m$d "
-  cd $d ; git pull
+  cd "$d" || exit ; git pull
   cd ../
 done
