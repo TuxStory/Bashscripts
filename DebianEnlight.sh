89a8444b100f8881a4ce9809a6c08973cc607cac
TuxStory <ajm.even@gmail.com>
2022-08-14 00:04:14 +0200

    enlight

--- DebianEnlight.sh
+++ DebianEnlight.sh
@@ -1,7 +1,7 @@
 ##############################
 # Date : 	13/08/2022   #
 # Auteur:	Antoine Even #
-# Version :	0.1          #
+# Version :	0.4          #
 ##############################
 
 #!/bin/bash
@@ -13,7 +13,13 @@ echo "==================================================="
 echo "          Installation d'enlightenment E17         "
 echo "==================================================="
 echo
-sudo apt install -y lightdm enlightenment eterm linux-headers-amd64 htop ncdu inxi ranger
+sudo apt install -y lightdm enlightenment linux-headers-amd64 terminology
+
+echo ">>> Multimedia :"
+sudo apt install -y audacious vlc
+
+echo ">>>> Utils :"
+sudo apt install -y htop ncdu ranger neofetch dfc bleachbit inxi
 
 echo "===================================================="
 echo "               Installation Terminée                "
