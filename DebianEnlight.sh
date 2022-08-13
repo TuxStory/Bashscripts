a8bea009e23ff505ee0ac934fd539b42263b8efd
TuxStory <ajm.even@gmail.com>
2022-08-13 23:13:03 +0200

    enlightement

---
+++ DebianEnlight.sh
@@ -0,0 +1,20 @@
+##############################
+# Date : 	13/08/2022   #
+# Auteur:	Antoine Even #
+# Version :	0.1          #
+##############################
+
+#!/bin/bash
+
+clear
+
+#installation des paquets
+echo "==================================================="
+echo "          Installation d'enlightenment E17         "
+echo "==================================================="
+echo
+sudo apt install -y lightdm enlightenment eterm linux-headers-amd64 htop ncdu inxi ranger
+
+echo "===================================================="
+echo "               Installation Terminée                "
+echo "===================================================="
