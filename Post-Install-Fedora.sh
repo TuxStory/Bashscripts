1127e3879f75c6317a65fe4b8c3ec29689eab1ac
TuxStory <ajm.even@gmail.com>
2023-02-13 22:12:18 +0100

    Post Install Fedora

---
+++ Post-Install-Fedora.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#####################################
+# Scrpit Fedora Post-Installation   #
+# 13/02/2023 ver 0.1                #
+# Antoine Even                      #
+#####################################
+
+EACCES=13 # Permission denied
+
+############### Couleurs
+GREEN='\033[1;32m'
+WHITE='\033[1;0m' #real white \033[1;37m
+RED='\033[0;91m'
+BLUE='\033[0;94m'
+MAGENTA='\033[0;95m'
+
+################ Root
+if [ "$UID" -ne 0 ]; then # Vous êtes ROOT
+  echo "Permission denied : you must be root."
+exit $EACCES
+fi
+
+############### Scrpit
+echo -e "${GREEN}>>> ${WHITE}Mise à jour du système."
+sudo dnf update
+
+################ Install RPM Fusion
+echo -e "\n${GREEN}>>> ${WHITE}Installation de RPM Fusion."
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install rpmfusion-free-release-tainted -y
+
+################ Install Administration
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares d'Administration."
+sudo dnf install htop dfc nano tmux screen htop nload nethogs ncdu nmon ranger vnstat smartmontools -y
+
+############### Install Internet
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares Internet."
+sudo dnf install firefox hexchat transmission-gtk -y
+
+############### Install Multimédia
+echo -e "\n${GREEN}>>> ${WHITE}Installation Multimédia."
+sudo dnf install audacious cmus vlc
+
+############### Install Codecs
+echo -e "\n${GREEN}>>> ${WHITE}Installation des codecs."
+sudo dnf install -y gstreamer1-plugins-{base,good,bad-free,good-extras,bad-free-extras,ugly-free} gstreamer1-libav
+sudo dnf install -y gstreamer1-plugins-{bad-freeworld,ugly}
+sudo dnf install -y libdvdcss
+
+################ Install Code
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares de Programmation."
+sudo dnf install -y codium gedit python3 git
