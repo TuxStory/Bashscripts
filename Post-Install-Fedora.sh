f086666d7171fa0a00a01bf60ff3fc8aab4aba71
TuxStory <ajm.even@gmail.com>
2023-02-14 23:26:06 +0100

    ver 0.6

--- Post-Install-Fedora.sh
+++ Post-Install-Fedora.sh
@@ -2,7 +2,7 @@
 
 #####################################
 # Scrpit Fedora Post-Installation   #
-# 14/02/2023 ver 0.3                #
+# 14/02/2023 ver 0.6                #
 # Antoine Even                      #
 #####################################
 
@@ -21,18 +21,24 @@ if [ "$UID" -ne 0 ]; then # Vous êtes ROOT
 exit $EACCES
 fi
 
+################ Clean dnf cache
+echo -e "${GREEN}>>> ${WHITE}Nettoyage du cache de DNF."
+dnf clean all
+
 ############### Scrpit
 echo -e "${GREEN}>>> ${WHITE}Mise à jour du système."
 sudo dnf update -y
 
 ################ Install RPM Fusion
 echo -e "\n${GREEN}>>> ${WHITE}Installation de RPM Fusion."
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install rpmfusion-free-release-tainted -y
 
 ################ Install Administration
 echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares d'Administration."
-sudo dnf install -y htop dfc nano tmux screen htop nload nethogs ncdu nmon ranger vnstat smartmontools
+sudo dnf install -y htop dfc nano tmux inxi screen htop nload nethogs neofetch ncdu nmon ranger vnstat \
+	smartmontools
 
 ############### Install Internet
 echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares Internet."
