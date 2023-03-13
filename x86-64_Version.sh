c6eac25d31034509bcfeca3d7864adc7cb4e01c1
TuxStory <ajm.even@gmail.com>
2023-03-13 23:56:22 +0100

    x64

--- x86-64_Version.sh
+++ x86-64_Version.sh
@@ -4,6 +4,7 @@ Version="0.0.2"
 CPU=$(cat /proc/cpuinfo | grep -i "^model name" | awk -F ": " '{print $2}' | head -1 | sed 's/ \+/ /g')
 flags=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2)
 
+supports_v1=''
 supports_v2='awk "/cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/ {found=1} END {exit !found}"'
 supports_v3='awk "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/ {found=1} END {exit !found}"'
 supports_v4='awk "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/ {found=1} END {exit !found}"'
