78818e86101ad21b3bfbba9b4b032dcc6a845330
TuxStory <ajm.even@gmail.com>
2023-03-14 23:55:41 +0100

    Version 0.0.4

--- x86-64_Version.sh
+++ x86-64_Version.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -eu
 
-Version="0.0.3"
+Version="0.0.4"
 CPU=$(cat /proc/cpuinfo | grep -i "^model name" | awk -F ": " '{print $2}' | head -1 | sed 's/ \+/ /g')
 flags=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2)
+Arch=$(uname -m)
+Vendor=$(cat /proc/cpuinfo | grep vendor | uniq | awk -F ": " '{print $2}')
 
 supports_v1='awk "/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/{found=1} END {exit !found}"'
 supports_v2='awk "/cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/ {found=1} END {exit !found}"'
@@ -10,6 +12,7 @@ supports_v3='awk "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&
 supports_v4='awk "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/ {found=1} END {exit !found}"'
 
 echo "$CPU"
+echo "$Vendor $Arch"
 echo "$flags" | eval $supports_v1 || exit 1 && echo "CPU supports x86-64-v1"
 echo "$flags" | eval $supports_v2 || exit 2 && echo "CPU supports x86-64-v2"
 echo "$flags" | eval $supports_v3 || exit 3 && echo "CPU supports x86-64-v3"
