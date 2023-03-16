3c7a2742cdbe516f90e584d705d02cf0c4881315
TuxStory <ajm.even@gmail.com>
2023-03-16 23:17:45 +0100

    DebianI3

--- DebianI3.sh
+++ DebianI3.sh
@@ -1,8 +1,8 @@
 ##############################
 # Date : 	18/08/2022   #
-# Maj  :	07/09/2022   #
+# Maj  :	16/03/2023   #
 # Auteur:	Antoine Even #
-# Version :	0.5          #
+# Version :	0.6          #
 ##############################
 
 #!/bin/bash
@@ -21,13 +21,13 @@ echo ">>> I3"
 sudo apt install -y lightdm i3 i3-wm kitty thunar nitrogen
 
 echo ">>> Multimedia :"
-sudo apt install -y audacious vlc eom
+sudo apt install -y audacious vlc eom hexchat transmission-gtk
 
 echo ">>>> Utils :"
-sudo apt install -y htop ncdu ranger neofetch dfc bleachbit inxi
+sudo apt install -y htop ncdu ranger neofetch dfc bleachbit inxi tmux nano nload nmon
 
 echo ">>>> Admin :"
-sudo apt install -y synaptic
+sudo apt install -y synaptic borgbackup
 
 echo ">>> Nettoyage :"
 sudo apt clean
