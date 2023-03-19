8322de8fd660f60e8ffd17c18df4c8b304e191d8
TuxStory <ajm.even@gmail.com>
2023-03-19 20:08:58 +0100

    I3

--- DebianI3.sh
+++ DebianI3.sh
@@ -7,8 +7,13 @@
 
 #!/bin/bash
 
-clear
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
 
+clear
 #installation des paquets
 echo "====================================="
 echo "          Installation de I3         "
@@ -40,7 +45,8 @@ sudo apt autoremove -y
 mkdir -p $HOME/Wallpaper
 cd $HOME/Wallpaper
 wget https://github.com/TuxStory/Bashscripts/blob/master/Wallpaper/debian2.png
-nitrogen --set-scaled $HOME/Wallpaper/debian2.png
+nitrogen --set-scaled debian2.png
+
 echo "exec --no-startup-id nitrogen --restore" >> $HOME/.config/i3/config
 
 echo "===================================================="
