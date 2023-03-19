92deff1ab33eeab5f957243b522695ca67097af7
TuxStory <ajm.even@gmail.com>
2023-03-19 20:46:03 +0100

    I3

--- DebianI3.sh
+++ DebianI3.sh
@@ -44,11 +44,11 @@ sudo apt autoremove -y
 #echo "exec --no-startup-id xrandr --output Virtual1 --mode 1360x768" >> $HOME/.config/i3/config
 
 #Wallpaper
-mkdir -p /home/$username/Wallpaper
-cd /home/$username/Wallpaper
-wget https://github.com/TuxStory/Bashscripts/Wallpaper/debian1.png
-wget https://github.com/TuxStory/Bashscripts/Wallpaper/debian2.png
-nitrogen --set-scaled debian2.png
+#mkdir -p /home/$username/Wallpaper
+#cd /home/$username/Wallpaper
+#wget https://github.com/TuxStory/Bashscripts/Wallpaper/debian1.png
+#wget https://github.com/TuxStory/Bashscripts/Wallpaper/debian2.png
+nitrogen --set-scaled /home/$username/Github/Wallpaper/debian2.png
 
 echo "exec --no-startup-id nitrogen --restore" >> /home/$username/.config/i3/config
 
