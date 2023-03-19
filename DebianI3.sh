35f3971ed1a948778f443761ffe1055fa6f40e75
TuxStory <ajm.even@gmail.com>
2023-03-19 22:43:43 +0100

    I3

--- DebianI3.sh
+++ DebianI3.sh
@@ -27,8 +27,8 @@ sudo apt update && sudo apt upgrade -y
 echo ">>> I3"
 sudo apt install -y lightdm i3 i3-wm kitty thunar nitrogen
 
-echo ">>> Multimedia :"
-sudo apt install -y audacious vlc eom hexchat transmission-gtk
+echo ">>> Multimedia & Internet :"
+sudo apt install -y audacious vlc eom hexchat transmission-gtk midori gedit
 
 echo ">>>> Utils :"
 sudo apt install -y htop ncdu ranger neofetch dfc bleachbit inxi tmux nano nload nmon
