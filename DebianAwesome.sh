b73b179c92e64a29b39237a6ab4be0b5bf4fba94
TuxStory <ajm.even@gmail.com>
2023-03-24 23:44:14 +0100

    Awesome

---
+++ DebianAwesome.sh
@@ -0,0 +1,62 @@
+##############################
+# Date : 	18/08/2022   #
+# Maj  :	20/03/2023   #
+# Auteur:	Antoine Even #
+# Version :	1.0          #
+##############################
+
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+clear
+
+#installation des paquets
+echo "====================================="
+echo "       Installation de Awesome       "
+echo "====================================="
+echo
+echo ">>> Mise à jour des dépots et du système :"
+sudo apt update && sudo apt upgrade -y
+
+echo ">>> I3"
+sudo apt install -y lightdm kitty thunar nitrogen awesome awesome-extra lxappearance compton
+
+echo ">>> Multimedia & Internet :"
+sudo apt install -y audacious vlc eom hexchat transmission-gtk midori gedit
+
+echo ">>>> Utils :"
+sudo apt install -y htop ncdu ranger neofetch dfc bleachbit inxi tmux nano nload nmon
+
+echo ">>>> Admin :"
+sudo apt install -y synaptic borgbackup openssh-server
+
+echo ">>> Nettoyage :"
+sudo apt clean
+sudo apt autoremove -y
+
+###############################################################
+# This part need i3-config-wizard to create .config/i3/config #
+# Re-run the script if i3 was run for the first time          #
+###############################################################
+
+#copy Awesome config file
+mkdir /home/$username/.config/awesome
+cp /etc/xdg/awesome/rc.lua /home/$username/.config/awesome/rc.lua
+
+#screen resolution for VirtualBox
+#echo "exec --no-startup-id xrandr --output Virtual1 --mode 1360x768" >> /home/$username/.config/i3/config
+
+#Wallpaper
+nitrogen --set-scaled Wallpaper/debian1.png --save
+
+#echo "exec --no-startup-id nitrogen --restore" >> /home/$username/.config/i3/config
+
+echo "===================================================="
+echo "               Installation Terminée                "
+echo "===================================================="
