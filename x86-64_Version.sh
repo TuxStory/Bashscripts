506fdc92eb29270c19f86e779d20a641ee84bb29
TuxStory <ajm.even@gmail.com>
2023-05-05 22:39:46 +0200

    version 0.5

--- x86-64_Version.sh
+++ x86-64_Version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-Version="0.0.4"
+Version="0.0.5"
 CPU=$(cat /proc/cpuinfo | grep -i "^model name" | awk -F ": " '{print $2}' | head -1 | sed 's/ \+/ /g')
 flags=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2)
 Arch=$(uname -m)
@@ -11,8 +11,11 @@ supports_v2='awk "/cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/ {found=1
 supports_v3='awk "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/ {found=1} END {exit !found}"'
 supports_v4='awk "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/ {found=1} END {exit !found}"'
 
-echo "$CPU"
-echo "$Vendor $Arch"
+echo "Marque       : $Vendor"
+echo "Architecture : $Arch"
+echo "Processeur   : $CPU"
+echo ""
+echo ">>> Architecture levels"
 echo "$flags" | eval $supports_v1 || exit 1 && echo "CPU supports x86-64-v1"
 echo "$flags" | eval $supports_v2 || exit 2 && echo "CPU supports x86-64-v2"
 echo "$flags" | eval $supports_v3 || exit 3 && echo "CPU supports x86-64-v3"
