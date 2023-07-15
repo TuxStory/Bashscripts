48e5ae05a36cff53a0a20939a7389f05fc2a35fd
TuxStory <ajm.even@gmail.com>
2023-07-15 21:03:38 +0200

    ping

--- CheckPing.sh
+++ CheckPing.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 IP="192.168.1.1"
-TIME="50"
+TIME="25"
 
 while :; do
-	result=$(ping -c1 $IP | grep -oP ".*temps=\K\d+")
-	#result=$(ping -c1 $IP | grep -oP ".*time=\K\d+")
-	echo $result
+	#result=$(ping -c1 $IP | grep -oP ".*temps=\K\d+")
+	result=$(ping -c1 $IP | grep -oP ".*time=\K\d+")
+	#echo $result
 	if [ $result -gt $TIME ]; then
         	echo -e "\033[0;31m>>> Warning ! Ping too High <<< $result ms\033[0m"
 	fi
