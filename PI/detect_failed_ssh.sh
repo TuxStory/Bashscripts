bac0c98595ab049480dd63cb9138a321d60476b6
TuxStory <ajm.even@gmail.com>
2023-11-05 18:36:56 +0100

    Version 0.1

--- PI/detect_failed_ssh.sh
+++ PI/detect_failed_ssh.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# grep "Failed password" /var/log/auth.log
+# Version : 0.1
+# Author  : Antoine Evne
+
+echo "================== "
+echo " Failed SSH Login "
+echo "=================="
+#Without journalctl
+#grep "Failed password" /var/log/auth.log
 #grep "Failed password" /var/log/auth.log | awk ‘{print $10}’ | uniq -c | sort -nr
 
-# journalctl
- journalctl _SYSTEMD_UNIT=ssh.service | egrep "Failed|Failure"
+#With journalctl
+journalctl _SYSTEMD_UNIT=ssh.service | grep "Failed|Failure"
 # journalctl _SYSTEMD_UNIT=sshd.service | egrep "Failed|Failure"  #In RHEL, CentOS
+
+echo "============"
+echo " Banned IPs "
+echo "============"
+grep 'Ban' /var/log/fail2ban.log*
