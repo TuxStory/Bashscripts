eaff70d3aef9ae2ec243c189ee2c275c3399da63
TuxStory <ajm.even@gmail.com>
2023-11-08 23:19:11 +0100

    0.5.1

--- detect_failed_ssh.sh
+++ detect_failed_ssh.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# Version : 0.5
+# Version : 0.5.1
 # Author  : Antoine Even
+
+JOURNAL="journalctl _SYSTEMD_UNIT=ssh.service"
 
 echo "================== "
 echo " Failed SSH Login "
 echo "=================="
 echo
+
 #Without journalctl
 #grep "Failed password" /var/log/auth.log
 #grep "Failed password" /var/log/auth.log | awk ‘{print $10}’ | uniq -c | sort -nr
-
 #With journalctl
-journalctl _SYSTEMD_UNIT=ssh.service | grep -E "Failed|Failure"
-# journalctl _SYSTEMD_UNIT=sshd.service | egrep "Failed|Failure"  #In RHEL, CentOS
+$JOURNAL | grep -E "Failed|Failure"
+# $JOURNAL | egrep "Failed|Failure"  #In RHEL, CentOS
+
 echo
-echo "============"
-echo " Banned IPs "
-echo "============"
+echo "=============================================="
+echo " Banned IPs (In history, Maybe not active now)"
+echo "=============================================="
 echo
 grep 'Ban' /var/log/fail2ban.log* 2>/dev/null
 echo
@@ -25,6 +28,7 @@ echo "==========================="
 echo " Successful Login Attempts "
 echo "==========================="
 echo
+
 #journalctl -u ssh.service -g opened
-journalctl _SYSTEMD_UNIT=ssh.service | grep -E "opened"
+$JOURNAL| grep -E "opened"
 echo
