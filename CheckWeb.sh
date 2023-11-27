16582604c8a45dc33d9b1916ec0697a591dcf493
TuxStory <ajm.even@gmail.com>
2023-11-27 23:44:59 +0100

    CheckPing

---
+++ CheckWeb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+############### Couleurs
+GREEN='\033[1;32m'
+WHITE='\033[1;0m' #real white \033[1;37m
+RED='\033[0;91m'
+
+############### Programme
+website=('www.google.be' 'www.yahoo.fr' 'www.distrowatch.com' 'www.debian.org' 'www.instagram.com' 'www.twitter.com' )
+
+for site in "${website[@]}"; do
+  ping -c1 $site > /dev/null 2>&1
+  RESULT=$?
+  if [ $RESULT -eq 0 ]; then
+    echo -e "$site : status [${GREEN} online ${WHITE}]"
+    #printf -e "%-25s %s\n" "$site" ": status [ online ]"
+  else
+    echo -e "$site : status [${RED} offline ${WHITE}]"
+  fi
+done
