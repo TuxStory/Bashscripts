91c01f5bf9268f9f522ccba747bcbf30d8464cfc
TuxStory <ajm.even@gmail.com>
2023-12-08 01:26:00 +0100

    Testing3

--- CarnetA/ZenityViewer.sh
+++ CarnetA/ZenityViewer.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 INPUT=addr.csv
-OLDIFS=$IFS
-IFS=";"
+#OLDIFS=$IFS
+#IFS=","
 
-while read Contact
+while read Prenom Nom Courriel Adresse Numero Ville Anniversaire
 do
-echo -e "$Contact"
-
-done < $INPUT|ans=$(zenity --width=600 --height=450 --list  --text "Contacts" --column "Prénom")
-echo "You chose: $ans"
+echo -e "$Prenom $Nom $Courriel $Adresse $Numero $Ville $Anniversaire"
+done < $INPUT|ans=$(zenity --width=800 --height=450 --list  --text "Contacts" --column "Prenom" --column "Nom") # --column "Courriel" --column "Adresse" --column "Numero" --column "Ville" --column "Anniversaire")
+echo "Vous avez choisi: $ans"
 
