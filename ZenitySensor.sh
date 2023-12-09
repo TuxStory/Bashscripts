ebfca497e1d2b54f0c313f6b3902fc71fc3e2c05
TuxStory <ajm.even@gmail.com>
2023-12-09 23:40:48 +0100

    test

---
+++ ZenitySensor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# show_cpu_temp.sh - Progress Dialog to show CPU temperature
+#
+echo
+ "Press [CTRL+C] to stop..."
+(
+while :; do
+  echo "# $(sensors | grep CPU)"
+  sensors | grep CPU | awk '{print substr($2,2,4) }'
+  sleep 5
+done ) | zenity --progress --title="CPU Temperature"
