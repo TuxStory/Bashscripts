064e074f9e3f1710d419f5a7c0f46efc5a34fa6e
TuxStory <ajm.even@gmail.com>
2024-02-04 02:25:56 +0100

    OpenSUSE-Auto

--- OpenSUSE-AutoRemove.sh
+++ OpenSUSE-AutoRemove.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#################
+# version : 1.0 #
+#################
+
+echo ">>> Vérification des paquets obsolètes."
 zypper packages --unneeded | awk -F'|' 'NR==0 || NR==1 || NR==2 || NR==3 || NR==4 {next} {print $3}' | grep -v Name | sudo xargs zypper remove --clean-deps
