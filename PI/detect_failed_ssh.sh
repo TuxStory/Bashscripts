bf1bd55a7d2d39ce392e6b930dc7800d0aa3009d
TuxStory <ajm.even@gmail.com>
2024-06-24 11:47:22 +0200

    Version 0.8.2

--- PI/detect_failed_ssh.sh
+++ PI/detect_failed_ssh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Version : 0.8.0
+# Version : 0.8.2
 # Author  : Antoine Even
 
-JOURNAL="journalctl _SYSTEMD_UNIT=ssh.service"
+JOURNAL="journalctl _SYSTEMD_UNIT=ssh.service" #Debian
+#JOURNAL="journalctl _SYSTEMD_UNIT=sshd.service" #Fedora Centos Rockylinux
 
 echo "================== "
 echo " Failed SSH Login "
@@ -11,8 +12,7 @@ echo "=================="
 echo
 
 #With journalctl
-$JOURNAL | grep -E "Failed|Failure"   #debian
-#$JOURNAL | egrep "Failed|Failure"  #In RHEL, CentOS
+$JOURNAL | grep -E "Failed|Failure"
 
 echo
 echo "==============================================="
@@ -22,9 +22,9 @@ echo
 grep 'Ban' /var/log/fail2ban.log* 2>/dev/null
 echo
 
-echo "=========================================="
-echo " Latest Successful login attempts from IP "
-echo "=========================================="
+echo "=============================================="
+echo " Latest Successful SSH login attempts from IP "
+echo "=============================================="
 echo
 $JOURNAL | grep -E "Accepted" | awk -F ": " '{print $1 $2}' | tail
 
