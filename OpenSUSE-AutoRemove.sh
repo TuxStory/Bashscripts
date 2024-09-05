e3a9798ec0765e7356c756ab15386ee3a6a1b5ce
TuxStory <ajm.even@gmail.com>
2024-09-06 00:05:38 +0200

    1.2

--- OpenSUSE-AutoRemove.sh
+++ OpenSUSE-AutoRemove.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #################
-# version : 1.1 #
+# version : 1.2 #
 #################
 
 ############### Couleurs
 GREEN='\033[1;32m'
+WHITE='\033[1;0m' #real white \033[1;37m
 
 echo -e "\n${GREEN}>>> ${WHITE} Vérification des paquets obsolètes."
 zypper packages --unneeded | awk -F'|' 'NR==0 || NR==1 || NR==2 || NR==3 || NR==4 {next} {print $3}' | grep -v Name | sudo xargs zypper remove --clean-deps
