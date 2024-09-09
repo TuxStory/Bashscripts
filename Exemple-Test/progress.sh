668d777fa505c037676dfa9c13aeed32d26bf1bc
TuxStory <ajm.even@gmail.com>
2024-09-09 23:48:02 +0200

    progress

--- Exemple-Test/progress.sh
+++ Exemple-Test/progress.sh
@@ -14,11 +14,11 @@ do
 done
 echo
 
-for ((k = 0; k <= 100 ; k++))
+for ((k = 0; k <= 50 ; k++))
 do
     echo -n "[ "
     for ((i = 0 ; i <= k; i++)); do echo -n "#"; done
-    for ((j = i ; j <= 100 ; j++)); do echo -n " "; done
+    for ((j = i ; j <= 50 ; j++)); do echo -n " "; done
     v=$((k * 1))
     echo -n " ] "
     echo -n "$v %" $'\r'
