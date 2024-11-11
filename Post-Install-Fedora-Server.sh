ea18156b94f33c6774777ddd0e6dda4ec438df85
TuxStory <ajm.even@gmail.com>
2024-11-11 01:43:15 +0100

    Fedora-server

---
+++ Post-Install-Fedora-Server.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+############################################
+# Scrpit Fedora Server Post-Installation   #
+# 06/06/2023 ver 0.0.1                     #
+# Antoine Even                             #
+############################################
+
+EACCES=13 # Permission denied
+
+############### Couleurs
+GREEN='\033[1;32m'
+WHITE='\033[1;0m' #real white \033[1;37m
+RED='\033[0;91m'
+BLUE='\033[0;94m'
+MAGENTA='\033[0;95m'
+
+################ Root
+if [ "$UID" -ne 0 ]; then # Vous êtes ROOT
+  echo "Permission denied : you must be root."
+exit $EACCES
+fi
+
+################ Clean dnf cache
+echo -e "${GREEN}>>> ${WHITE}Nettoyage du cache de DNF."
+dnf clean packages # dnf clean all = Efface tout = brutal
+
+############### Scrpit
+echo -e "${GREEN}>>> ${WHITE}Mise à jour du système."
+sudo dnf update -y
+
+################ Install RPM Fusion
+#echo -e "\n${GREEN}>>> ${WHITE}Installation de RPM Fusion."
+#sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+#	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#sudo dnf install rpmfusion-free-release-tainted -y
+
+################ Install Administration
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares d'Administration."
+sudo dnf install -y htop dfc fastfetch nano tmux inxi screen htop nload nethogs ncdu nmon ranger vnstat \
+	smartmontools duf ethtool
