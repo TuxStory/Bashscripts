892e05307a84e1443bd606fda3b75a991427ed36
TuxStory <ajm.even@gmail.com>
2024-11-17 02:18:23 +0100

    Add line

--- x86-64_Version.sh
+++ x86-64_Version.sh
@@ -16,9 +16,10 @@ supports_v2='awk "/cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/ {found=1
 supports_v3='awk "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/ {found=1} END {exit !found}"'
 supports_v4='awk "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/ {found=1} END {exit !found}"'
 
-echo "Marque       : $Vendor"
-echo "Architecture : $Arch"
-echo "Processeur   : $CPU"
+echo -e "${GREEN}>>>${WHITE} Architecture Infos :"
+echo -e "Marque       : $Vendor"
+echo -e "Architecture : $Arch"
+echo -e "Processeur   : $CPU"
 echo ""
 echo -e "${GREEN}>>>${WHITE} Architecture levels :"
 echo "$flags" | eval $supports_v1 || exit 1 && echo -e "CPU supports x86-64-v1 [  ${GREEN}OK  ${WHITE}]"
