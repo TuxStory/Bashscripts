a0eff2d658b804b45fa151bb7cf468758b4bfa7f
TuxStory <ajm.even@gmail.com>
2024-11-27 00:42:09 +0100

    aliases

---
+++ aliases.sh
@@ -0,0 +1,12 @@
+# Exemples d'alias
+# Debian .bash_aliases
+
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+alias history='history | less'
+alias ip='ip -c'
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -hl'
+alias ls='ls --color=auto'
