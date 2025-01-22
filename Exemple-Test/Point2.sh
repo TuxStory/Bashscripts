2674787485fb512085962827104628b7355db79c
TuxStory <ajm.even@gmail.com>
2025-01-22 23:30:16 +0100

    update

--- Exemple-Test/Point2.sh
+++ Exemple-Test/Point2.sh
@@ -10,7 +10,7 @@ do
     for j in {0..10} {10..0}
       do
       if [ $j -eq $i ] ; then
-        echo -n "o  " #+1 espace pour effacer le poit precedent au retour.
+        echo -n "o " #+1 espace pour effacer le poit precedent au retour.
         sleep 0.05
         #echo $i $j
       else
