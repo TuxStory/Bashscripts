2f543531779f29e72c4ddf673d43ccc9a856b13c
TuxStory <ajm.even@gmail.com>
2025-01-29 00:25:10 +0100

    fastz

--- PI/fastfetch_download.sh
+++ PI/fastfetch_download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ~/Téléchargements/
-https://github.com/fastfetch-cli/fastfetch/releases/download/2.35.0/fastfetch-linux-aarch64.deb
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.35.0/fastfetch-linux-aarch64.deb
 echo "OK"
