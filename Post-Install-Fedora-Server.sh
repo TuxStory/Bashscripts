778fb339ebe6bdd02d2bb9e3d3b76bd92fcfae03
TuxStory <ajm.even@gmail.com>
2025-02-16 23:58:30 +0100

    0.0.5

--- Post-Install-Fedora-Server.sh
+++ Post-Install-Fedora-Server.sh
@@ -2,7 +2,7 @@
 
 ############################################
 # Scrpit Fedora Server Post-Installation   #
-# 06/06/2023 ver 0.0.4                     #
+# 06/06/2023 ver 0.0.5                     #
 # Antoine Even                             #
 ############################################
 
@@ -28,12 +28,6 @@ dnf clean packages # dnf clean all = Efface tout = brutal
 ############### Scrpit
 echo -e "${GREEN}>>> ${WHITE}Mise à jour du système."
 sudo dnf update -y
-
-################ Install RPM Fusion
-#echo -e "\n${GREEN}>>> ${WHITE}Installation de RPM Fusion."
-#sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-#	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-#sudo dnf install rpmfusion-free-release-tainted -y
 
 ################ Install Administration
 echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares d'Administration."
