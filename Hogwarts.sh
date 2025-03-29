91e5d68adee7592018f0ea257cbbdd6acc5dda7c
TuxStory <ajm.even@gmail.com>
2025-03-29 23:37:12 +0100

    Hogwarts update

--- Hogwarts.sh
+++ Hogwarts.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Name : Hogwarts.sh
-# Date : 28/03/2035
-# version : 0.1
+# Date : 29/03/2035
+# version : 0.2
 # Delete coredump file systemd
 
 
 ls /var/lib/systemd/coredump/
+echo "Deleting the file"
+sudo rm /var/lib/systemd/coredump/*
+echo "File deleted ..."
