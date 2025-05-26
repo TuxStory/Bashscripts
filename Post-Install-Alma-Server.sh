b1305574d615f278b082775362489482cbd9fe4b
TuxStory <ajm.even@gmail.com>
2025-05-26 23:49:29 +0200

    version 0.8

--- Post-Install-Alma-Server.sh
+++ Post-Install-Alma-Server.sh
@@ -2,7 +2,7 @@
 
 #######################################
 # Scrpit AlmaRocky Post-Installation  #
-# 10/03/2025 ver 0.0.7                #
+# 26/05/2025 ver 0.0.8                #
 # Antoine Even                        #
 #######################################
 
@@ -19,7 +19,7 @@ MAGENTA='\033[0;95m'
 
 ################ Root
 if [ "$UID" -ne 0 ]; then # Vous êtes ROOT
-  echo "Permission denied : you must be root."
+  echo -e "Permission denied : you must be ${RED}root${WHITE}."
 exit $EACCES
 fi
 
