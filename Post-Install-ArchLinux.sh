6536b4aeb1924b70e31f9f30ff071580683c6637
TuxStory <ajm.even@gmail.com>
2025-06-29 23:16:09 +0200

    archlinux

---
+++ Post-Install-ArchLinux.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#######################################
+# Scrpit ArchLinux Post-Installation  #
+# 28/06/2025 ver 0.0.2                #
+# Antoine Even                        #
+#######################################
+
+EACCES=13 # Permission denied
+
+############### Couleurs
+GREEN='\033[1;32m'
+WHITE='\033[1;0m' #real white \033[1;37m
+RED='\033[0;91m'
+BLUE='\033[0;94m'
+MAGENTA='\033[0;95m'
+
+################ Root
+if [ "$UID" -ne 0 ]; then # Vous êtes ROOT
+  echo "Permission denied : you must be root."
+exit $EACCES
+fi
+
+################ Clean cache
+echo -e "${GREEN}>>> ${WHITE}Nettoyage du cache des paquets."
+sudo pacman -S --noconfirm pacman-contrib
+sudo paccache -r
+
+############### Scrpit
+echo -e "${GREEN}>>> ${WHITE}Mise à jour du système."
+sudo pacman -Suy --noconfirm
+
+################ Install Administration
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares d'Administration."
+sudo pacman -S --noconfirm btop borgbackup dfc fastfetch nano nmap tmux timeshift inxi screen htop ncdu nload nethogs nmon nvtop ranger \
+	smartmontools duf ethtool vnstat
+
+############### Install Internet
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Softwares Internet."
+sudo pacman -S --noconfirm firefox filezilla hexchat transmission-gtk thunderbird
+
+############### Install Multimédia
+echo -e "\n${GREEN}>>> ${WHITE}Installation Multimédia."
+sudo pacman -S --noconfirm audacious cava cmus mpg123 vlc ffmpegthumbnailer gthumb gimp
+
+################ Install jeux
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Jeux."
+sudo pacman -S --noconfirm luanti gnome-games shattered-pixel-dungeon
+
+################ Install Backgrounds
+echo -e "\n${GREEN}>>> ${WHITE}Installation des Wallpapers."
+sudo pacman -S --noconfirm archlinux-wallpapers
+
+############### Gnome
+echo -e "\n${GREEN}>>> ${WHITE}Installation des extensions de Gnome"
+sudo pacman -S --noconfirm gnome-shell-extension-appindicator
