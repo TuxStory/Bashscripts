3cf3e7340f5d0f72b2b068cb0c87a3b482548caa
TuxStory <ajm.even@gmail.com>
2025-07-17 23:52:50 +0200

    Affichage

--- PI/Old-Kernels.sh
+++ PI/Old-Kernels.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+################ Couleurs
+GREEN='\033[1;32m'
+WHITE='\033[1;0m' #real white \033[1;37m
+RED='\033[0;91m'
+BLUE='\033[0;94m'
+MAGENTA='\033[0;95m
+
+################ Affichage
+echo -e "\n${GREEN}>>> ${WHITE}Nettoyage des anciens noyaux!"
+
+################ Programme
 echo ">>> Apt List"
 apt list -i linux-image* linux-headers*
 
