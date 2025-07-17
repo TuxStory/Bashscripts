a2cc6194ae286e96595e13c017093c7e45fe4164
TuxStory <ajm.even@gmail.com>
2025-07-18 00:12:50 +0200

    Correction

--- PI/Old-Kernels.sh
+++ PI/Old-Kernels.sh
@@ -5,7 +5,7 @@ GREEN='\033[1;32m'
 WHITE='\033[1;0m' #real white \033[1;37m
 RED='\033[0;91m'
 BLUE='\033[0;94m'
-MAGENTA='\033[0;95m
+MAGENTA='\033[0;95m'
 
 ################ Affichage
 echo -e "\n${GREEN}>>> ${WHITE}Nettoyage des anciens noyaux!"
