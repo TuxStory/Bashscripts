dfed8c9b783fdc69f0b3f272cbd077640fe40de5
TuxStory <ajm.even@gmail.com>
2025-09-04 23:34:19 +0200

    Ponctuation

--- Hogwarts.sh
+++ Hogwarts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Name : Hogwarts.sh
-# Date : 29/03/2025
-# version : 0.5
+# Date : 04/09/2025
+# version : 0.6
 # Delete coredump file systemd
 
 
-echo -e "\n>>> Hogwarts tmp file cleaner"
+echo -e "\n>>> Hogwarts tmp file cleaner."
 
 ls /var/lib/systemd/coredump/
-echo -e "\n>>> Deleting the file"
+echo -e "\n>>> Deleting the file."
 sudo rm /var/lib/systemd/coredump/*
 
 if [ $? -eq 0 ]; then
