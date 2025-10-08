ef1ae784bcc77d13e172ac067b44a95e2146bda9
TuxStory <ajm.even@gmail.com>
2025-10-09 00:19:44 +0200

    colors

--- Hogwarts.sh
+++ Hogwarts.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Name : Hogwarts.sh
-# Date : 04/09/2025
-# version : 0.6
+# Date : 09/10/2025
+# version : 0.7
 # Delete coredump file systemd
 
+## Couleurs ##############
+GREEN='\033[1;32m'
+WHITE='\033[1;0m' #real white \033[1;37m
+RED='\033[0;91m'
+BLUE='\033[0;94m'
+MAGENTA='\033[0;95m'
 
-echo -e "\n>>> Hogwarts tmp file cleaner."
+
+echo -e "${BLUE}>>> ${WHITE} Hogwarts tmp file cleaner."
 
 ls /var/lib/systemd/coredump/
-echo -e "\n>>> Deleting the file."
+echo -e "${GREEN}>>> ${WHITE} Deleting the file."
 sudo rm /var/lib/systemd/coredump/*
 
 if [ $? -eq 0 ]; then
-    echo ">>> Success ! File deleted ..."
+    echo -e "${GREEN}>>> ${WHITE} Success ! File deleted ..."
 else
-    echo ">>> An Error Occurred !"
+    echo -e "${RED}>>> ${WHITE} An Error Occurred !"
 fi
 
