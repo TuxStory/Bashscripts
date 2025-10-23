df70b14cdb7664ccf4778b7780dbeaf147061e83
TuxStory <ajm.even@gmail.com>
2025-10-24 00:42:17 +0200

    version 0.0.4

--- Post-Install-Ubuntu.sh
+++ Post-Install-Ubuntu.sh
@@ -2,7 +2,7 @@
 
 #####################################
 # Scrpit Ubuntu Post-Installation   #
-# 22/10/2025 ver 0.0.3              #
+# 22/10/2025 ver 0.0.4              #
 # Antoine Even                      #
 #####################################
 
@@ -17,7 +17,7 @@ MAGENTA='\033[0;95m'
 
 ################ Root
 if [ "$UID" -ne 0 ]; then # Vous êtes ROOT
-  echo -e "Permission denied : you must be ${RED}root${WHITE}."
+  echo -e "Accès refusé : vous devez être ${RED}root${WHITE}."
 exit $EACCES
 fi
 
