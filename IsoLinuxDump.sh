69486fa607b7e2a98cbde2a34ba26cef0c0cca97
TuxStory <ajm.even@gmail.com>
2025-11-12 23:48:03 +0100

    Update

--- IsoLinuxDump.sh
+++ IsoLinuxDump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-####################
-# NEED TO BE FIXEX #
-####################
+#######################
+# NEED TO BE FIXEX :) #
+#######################
 
 echo "Exemple de site possible : "
 echo "www.debian.org"
