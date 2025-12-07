e35614b651201a35ce9abec5124fddac7c69ea34
TuxStory <ajm.even@gmail.com>
2025-12-07 22:42:06 +0100

    update

--- OpenSUSE-AutoRemove.sh
+++ OpenSUSE-AutoRemove.sh
@@ -11,5 +11,4 @@ WHITE='\033[1;0m' #real white \033[1;37m
 echo -e "\n${GREEN}>>> ${WHITE} Vérification des paquets obsolètes."
 zypper packages --unneeded | awk -F'|' 'NR==0 || NR==1 || NR==2 || NR==3 || NR==4 {next} {print $3}' | grep -v Name | sudo xargs zypper remove --clean-deps
 
-
 ## A verifié ne fonctionne peut-être plus
